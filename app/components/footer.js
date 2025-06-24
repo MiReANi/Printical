e9101efba3da0b47e73f5e7a620f70876e335c51
Your Name <you@example.com>
2025-06-24 13:30:09 +0300

    Muokkauksia

--- app/components/footer.js
+++ app/components/footer.js
@@ -3,29 +3,33 @@ import Link from 'next/link'
 
 export default function Footer(){
     return(
-        <div className="bottom-0 bg-darkblue p-20 text-white flex flex-col md:flex-row">
-            <div>
-                <p>Printical Oy</p>
-                <p>3509458-8</p>
-                <p>printical.fi@gmail.com</p>
-                <p>+358 440310604</p>
-            </div>
-            <div className='md:ml-10'>
-                <p>Karjalankatu 12</p>
-                <p>28130, Pori</p>
-                <div className='md:flex flex-row mt-5'>
-                    <Link href="https://www.instagram.com/printical.fi/"
-                    ><Instagram
-                    size={32}/>
-                    </Link>
-                    <Link href="https://www.facebook.com/profile.php?id=61574020655204"
-                    ><Facebook
-                    size={32}/>
-                    </Link>
+        <div className='bottom-0 bg-darkblue p-10 text-white'>
+            <div className="flex flex-col md:flex-row">
+                <div>
+                    <p>Printical Oy</p>
+                    <p>3509458-8</p>
+                    <p>printical.fi@gmail.com</p>
+                    <p>+358 440310604</p>
                 </div>
+                <div className='md:ml-10'>
+                    <p>Karjalankatu 12</p>
+                    <p>28130, Pori</p>
+                    <div className='md:flex flex-row mt-5'>
+                        <Link href="https://www.instagram.com/printical.fi/"
+                        ><Instagram
+                        size={32}/>
+                        </Link>
+                        <Link href="https://www.facebook.com/profile.php?id=61574020655204"
+                        ><Facebook
+                        size={32}/>
+                        </Link>
+                    </div>
+                    
+                </div>
+                
+
             </div>
-
-
+            <p className='text-center'>©2025 Printical Oy</p>
         </div>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,12 @@
 'use client'
 import Image from 'next/image'
 import Placeholder from '/public/printicaltaustakuva.jpg'
-import ModelForm from './components/modelform'
-import ProductBox from './productbox.js'
-import Link from 'next/link'
 import FrontText from '/public/teksti etusivu.svg'
 import Navbar from './components/navbar'
-
-
-//import ProductManager from './product.js'
-/*import {db} from './lib/db'
-
-async function Database(){
-  let allPosts = await db.select().from("tuotteet")
-  return (
-    <ul>
-      {allPosts.map((post) => (
-        <li key={post.id}>{post.title}</li>
-      ))}
-    </ul>
-  )
-}*/
-
+import Skull from '/public/kallo.png'
+import Link from 'next/link'
+import Materials from '/public/uudensukupolvenmateriaaleja.svg'
+import PrinticalLogo from '/public/printicallogo tumma.svg'
 
 export default function Home() {
   return (
@@ -34,18 +19,32 @@ export default function Home() {
 
             <Image className='md:mx-10 md:my-10 absolute md:top-[250px] top-20 md:left-[125px]' src={FrontText} alt='FrontText'/>
         </div>
-        <div className='mt-20 md:grid grid-cols-2 bg-darkblue text-sm text-white'>
-            <div className='p-5 md:mx-0 md:border-r-4 md:border-b-0 border-b-4 border-blue md:p-20 md:ml-20 md:text-lg/8'>
-              <p>Printical on 3D-tulostamiseen erikoistunut yritys, joka tarjoaa luovia ratkaisuja täysin asiakkaan toiveiden ja tarpeiden mukaan. Pyrimme päivittäin haastamaan itsemme luoden jotain käytännöllistä ja kaunista asiakkaittemme iloksi.</p>
-            </div>
-            <div className='p-5 md:mx-0 md:p-20 md:mr-20 md:text-lg/8'>
-              <p>Kaikki tuotteemme suunnitellaan ja valmistetaan Suomessa mittatilauksena 3D-tulostaen ja käsityönä kooten. Olemme joustava ja innovatiivinen toimija, jonka tuotevalikoiman rajoituksena on ainoastaan sinun mielikuvituksesi!</p>
-            </div>
+          <div className='flex flex-row justify-center md:mt-10'>
+            <Link href="/information">
+            <p className='bg-darkblue md:mt-2 text-white p-10 font-extrabold text-6xl'>TIETOA MEISTÄ</p>
+            </Link>
+            <Image className='md:mx-10' src={PrinticalLogo} alt='PrinticalLogo'/>
+            <Link href="/contact">
+            <p className='bg-darkblue md:mt-2 text-white p-10 font-extrabold text-6xl'>OTA YHTEYTTÄ</p>
+            </Link>
+          </div>
+          <div className='mt-10 md:grid grid-cols-2 bg-darkblue text-sm text-white border-blue md:border-b-4 md:border-t-4'>
+              <div className='p-5 md:mx-0 md:border-r-4 md:border-b-0 border-b-4 border-blue md:p-20 md:ml-20 md:text-lg/8'>
+                <p>Printical on 3D-tulostamiseen erikoistunut yritys, joka tarjoaa luovia ratkaisuja täysin asiakkaan toiveiden ja tarpeiden mukaan. Pyrimme päivittäin haastamaan itsemme luoden jotain käytännöllistä ja kaunista asiakkaittemme iloksi.</p>
+              </div>
+              <div className='p-5 md:mx-0 md:p-20 md:mr-20 md:text-lg/8'>
+                <p>Kaikki tuotteemme suunnitellaan ja valmistetaan Suomessa mittatilauksena 3D-tulostaen ja käsityönä kooten. Olemme joustava ja innovatiivinen toimija, jonka tuotevalikoiman rajoituksena on ainoastaan sinun mielikuvituksesi!</p>
+              </div>
             
             
 
 
 
+          </div>
+          <div className='md:grid grid-cols-3'>
+              <Image src={Skull} alt='Skullplaceholder'/>
+              <Image className='md:mx-12 md:mt-[200px]' src={Materials} alt='Materials'/>
+              <Image src={Skull} alt='Skull'/>
           </div>
       </div>
 
